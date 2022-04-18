d14db96ad3159047de9f14e32e8076812ad6f2d6
Andrik <97067440+AndiGit101@users.noreply.github.com>
2022-04-18 01:41:20 +0000

    Implemented ROT sift and added more to Caesar shift function

--- main.py
+++ main.py
@@ -1,104 +1,153 @@
-'''
-This project is the codebreaker encrypted and decrypter for content written in py.
-Functions use ceaser shift and reverse ceaser algorithm
-'''
-
-import fileinput
 import os.path
-import math
 import time
 
-############################
+###########################
 
+#Standard key alphabet to manage encryption and decryptions
+key_alphabet = {"a": "a", "b": "b", "c": "c", "d": "d", "e": "e", "f": "f", "g": "g", "h": "h", "i": "i", "j": "j",
+                "k": "k", "l": "l", "m": "m", "n": "n", "o": "o", "p": "p", "q": "q", "r": "r", "s": "s", "t": "t",
+                "u": "u", "v": "v", "w": "w", "x": "x", "y": "y", "z": "z"}
+
+#Alphabet
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+new_alpha = ""
+
+
+# Helper functions
+# ==========================================#
+def left(index):
+
+    return alphabet[-index:] + alphabet[:-index]
+
+
+def right(index):
+
+    return alphabet[index:] + alphabet[:index]
+
+#==============================================#
 
 # Encoding algorithms
-def caesarShiftEncrypt(target_file1 , choice):
-	
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    encrypted_message = ""
-	
-    key_alphabet = {"a": "a", "b": "b", "c": "c", "d": "d", "e": "e", "f": "f", "g": "g",  "h":"h", "i": "i", "j": "j", "k": "k", "l": "l", "m": "m", "n": "n", "o": "o", "p": "p", "q": "q", "r": "r", "s": "s", "t": "t", "u": "u", "v": "v", "w": "w", "x": "x", "y": "y", "z": "z"}
-	
-	    
-		
+def caesarShiftEncrypt(target_file1, choice):
+    message = ""
+
     if choice == "encrypt":
-	
-		
-	    modified_alphabet = alphabet[-3:] + alphabet[:-3]  # Shift right 3
-	
-	    # Append new items to the alphabet list
-	    for i in range(len(key_alphabet)):
-	        key_alphabet[alphabet[i]] = modified_alphabet[i]
-	
-	
-	    #Perform file operation and read content to encrypt
-	    open_file = open(target_file1, "r")
-	    content = open_file.read()
-	    open_file.close()#Save memory
-	
-	
-	    if content != "" or content is not None:
-	
-	        for i in range(len(content)):
-	
-	            if content[i] == " ":
-	
-	                encrypted_message += " "
-	            else:
-	
-	                encrypted_message += str(key_alphabet.get(content[i]))
-	
-	    print("Encrypted message\n" + encrypted_message)
-	
+
+        new_alpha= left(3)
+
+        # Append new items to the alphabet list
+        for i in range(len(key_alphabet)):
+            key_alphabet[alphabet[i]] = new_alpha[i]
+
+            # Perform file operation and read content to encrypt
+        open_file = open(target_file1, "r")
+        content = open_file.read()
+        open_file.close()  # Save memory
+
+        if content != "" or content is not None:
+
+            for i in range(len(content)):
+
+                if content[i] == " ":
+                    #If there is a space
+                    message += " "
+                else:
+
+                    message += str(key_alphabet.get(content[i]))
+
+            time.sleep(1)
+            print("Encrypted message is: " + message)
+
 
     elif choice == "decrypt":
 
-		 
-	      for i in range(3):
-	
-		      #left
-		      for i in range(len(alphabet) -1):
-		
-		        temp = alphabet[i+1]
-		        alphabet[i+1] = alphabet[i]
-		        alphabet[i] = temp
 
+        new_alpha = right(3)
 
-			 # Append new items to the alphabet list
-    for i in range(len(key_alphabet)):
-				   
-            key_alphabet[alphabet[i]] = modified_alphabet[i]
+        # Append new items to the alphabet list
+        for i in range(len(key_alphabet)):
+            key_alphabet[alphabet[i]] = new_alpha[i]
 
-	
-    if content != "" or content is not None:
-	
-	        for i in range(len(content)):
-	
-	            if content[i] == " ":
-	
-	                encrypted_message += " "
-	            else:
-	
-	                encrypted_message += str(key_alphabet.get(content[i]))
+            # Perform file operation and read content to encrypt
+            open_file = open(target_file1, "r")
+            content = open_file.read()
+            open_file.close()  # Save memory
 
+        if content != "":
 
-	
-    
-	  print("Encrypted message\n" + encrypted_message)
-	
-      
+            for i in range(len(content)):
+
+                if content[i] == " ":
+
+                    message += " "
+                else:
+
+                    message += str(key_alphabet.get(content[i]))
+
+        time.sleep(1)
+        print("Encrypted message\n" + message)
 
 
 
-def ROTShift(target_file2, index2):
-    print("File could not be decrypted. File not found")
 
-    key_alphabet = {"a": "a", "b": "b", "c": "c", "d": "d", "e": "e", "f": "f", "g": "g",
-                    "h": "h", "i": "i", "j": "j", "k": "k", "l": "l", "m": "m", "n": "n", "o": "o", "p": "p", "q": "q",
-                    "r": "r", "s": "s", "t": "t", "u": "u", "v": "v", "w": "w", "x": "x", "y": "y", "z": "z"}
 
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+def ROTShift(target_file2, choice , shift):
+    message = ""
+
+    if choice == "encrypt":
+
+        new_alpha = left(shift)
+
+        # Append new items to the alphabet list
+        for i in range(len(key_alphabet)):
+            key_alphabet[alphabet[i]] = new_alpha[i]
+
+            # Perform file operation and read content to encrypt
+        open_file = open(target_file2, "r")
+        content = open_file.read()
+        open_file.close()  # Save memory
+
+        if content != "":
+
+            for i in range(len(content)):
+
+                if content[i] == " ":
+                    # If there is a space
+                    message += " "
+                else:
+
+                    message += str(key_alphabet.get(content[i]))
+
+            time.sleep(1)
+            print("Encrypted message is: " + message)
+
+
+    elif choice == "decrypt":
+
+        new_alpha = right(shift)
+
+        # Append new items to the alphabet list
+        for i in range(len(key_alphabet)):
+            key_alphabet[alphabet[i]] = new_alpha[i]
+
+            # Perform file operation and read content to encrypt
+            open_file = open(target_file2, "r")
+            content = open_file.read()
+            open_file.close()  # Save memory
+
+        if content != "":
+
+            for i in range(len(content)):
+
+                if content[i] == " ":
+
+                    message += " "
+                else:
+
+                    message += str(key_alphabet.get(content[i]))
+
+        time.sleep(1)
+        print("Encrypted message\n" + message)
 
 
 
@@ -123,24 +172,22 @@ if __name__ == '__main__':
 
         if type == 1:
 
-            ROT_OPTION = int(input("Which ROT function would you like to implement\n1:ROT132:Custom\n"))
+            ROT_OPTION = int(input("Which ROT function would you like to implement\n1:ROT13\n2:Custom\n"))
 
             if ROT_OPTION == 1:
 
-                ROTShift(file, 13)
+                ROTShift(file,"encrypt", 13)
 
             elif ROT_OPTION == 2:
 
                 shift = int(input("Enter shift:\n"))
 
-                ROTShift(file, shift)  # ROTX call tp encrypt
+                ROTShift(file, "encrypt", shift)  # ROTX call tp encrypt
 
 
         elif type == 2:
 
-            caesarShiftEncrypt(file , "encrypt")
-
-
+            caesarShiftEncrypt(file, "encrypt")
 
 
 
@@ -153,23 +200,23 @@ if __name__ == '__main__':
             time.sleep(1)
             file = input("Enter a file to decrypt\n")
 
-        type = int(input("Encrypt with ROTx or Caesar shift?\n1:ROTX Decrypt\n2: Caesar Decrypt"))
+        type = int(input("Encrypt with ROTx or Caesar shift?\n1:ROTX Decrypt\n2: Caesar Decrypt\n"))
 
         if type == 1:
 
-            ROT_OPTION = int(input("Which ROT function would you like to implement\n1:ROT132:Custom"))
+            ROT_OPTION = int(input("Which ROT function would you like to implement\n1:ROT13\n2:Custom\n"))
 
             if ROT_OPTION == 1:
 
-                ROTShift(file, 13)
+                ROTShift(file, "decrypt", 13)
 
             elif ROT_OPTION == 2:
 
                 shift = int(input("Enter shift:\n"))
 
-                ROTShift(file, shift)  # ROTX call tp encrypt
+                ROTShift(file, "decrypt" , shift)  # ROTX call tp encrypt
 
 
         elif type == 2:
 
-            caesarShiftEncrypt(file , "decrypt")
+            caesarShiftEncrypt(file, "decrypt")
